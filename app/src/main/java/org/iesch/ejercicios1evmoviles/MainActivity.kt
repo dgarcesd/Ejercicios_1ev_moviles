1b67894a978dadf5c0a0bdd33dc256ea8a8c4878
dgarcesd <dgarcesd@iesch.org>
2023-11-21 05:56:52 +0100

    Implementar la calculadora

--- app/src/main/java/org/iesch/ejercicios1evmoviles/MainActivity.kt
+++ app/src/main/java/org/iesch/ejercicios1evmoviles/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import org.iesch.ejercicios1evmoviles.databinding.ActivityMainBinding
 import org.iesch.ejercicios1evmoviles.ejercicios.CalculadoraActivity
 
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,33 +17,33 @@ class MainActivity : AppCompatActivity() {
             intent = Intent(this, CalculadoraActivity::class.java)
             startActivity(intent)
         }
-        binding.rellaySuperheroes.setOnClickListener {
-            intent = Intent(this, SuperHeroesActivity::class.java)
-            startActivity(intent)
-        }
-        binding.rellayEdadCanina.setOnClickListener {
-            intent = Intent(this, EdadCaninaActivity::class.java)
-            startActivity(intent)
-        }
-        binding.rellayQuizzes.setOnClickListener {
-            intent = Intent(this, QuizzesActivity::class.java)
-            startActivity(intent)
-        }
-        binding.rellayGaleria.setOnClickListener {
-            intent = Intent(this, GaleriaActivity::class.java)
-            startActivity(intent)
-        }
-        binding.rellayMapas.setOnClickListener {
-            intent = Intent(this, MapasActivity::class.java)
-            startActivity(intent)
-        }
-        binding.rellayRestaurantes.setOnClickListener {
-            intent = Intent(this, RestaurantesActivity::class.java)
-            startActivity(intent)
-        }
-        binding.rellaySettings.setOnClickListener {
-            intent = Intent(this, SettingsActivity::class.java)
-            startActivity(intent)
-        }
+//        binding.rellaySuperheroes.setOnClickListener {
+//            intent = Intent(this, SuperHeroesActivity::class.java)
+//            startActivity(intent)
+//        }
+//        binding.rellayEdadCanina.setOnClickListener {
+//            intent = Intent(this, EdadCaninaActivity::class.java)
+//            startActivity(intent)
+//        }
+//        binding.rellayQuizzes.setOnClickListener {
+//            intent = Intent(this, QuizzesActivity::class.java)
+//            startActivity(intent)
+//        }
+//        binding.rellayGaleria.setOnClickListener {
+//            intent = Intent(this, GaleriaActivity::class.java)
+//            startActivity(intent)
+//        }
+//        binding.rellayMapas.setOnClickListener {
+//            intent = Intent(this, MapasActivity::class.java)
+//            startActivity(intent)
+//        }
+//        binding.rellayRestaurantes.setOnClickListener {
+//            intent = Intent(this, RestaurantesActivity::class.java)
+//            startActivity(intent)
+//        }
+//        binding.rellaySettings.setOnClickListener {
+//            intent = Intent(this, SettingsActivity::class.java)
+//            startActivity(intent)
+//        }
     }
 }
